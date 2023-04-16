2eac006246a1a4d0e0391d01acc06bdb16f2e72e
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-16 21:35:04 +0530

    Simple program on Array elements

--- PracticeProblemsonArray/Program.cs
+++ PracticeProblemsonArray/Program.cs
@@ -11,6 +11,7 @@ namespace PracticeProblemsonArray
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome message for array's practice problems");
+            ConutDuplicatevalues.arrayInfo();
             Console.ReadLine();
         }
     }
